440bc508a7c077a82320ace5acd73e6eb30fe7d4
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2023-01-09 18:14:31 -0500

    update to keep consistent with work

--- src/languages/lang.js
+++ src/languages/lang.js
@@ -4,19 +4,28 @@
   Author: AdamRaichu
 */
 
-/**
- * @param {import("../highlight").HLJSApi} hljs
- */
 export default function(hljs) {
   return {
     name: "lang",
     case_insensitive: false,
-    keywords: "",
+    // §[0-9a-gk-or]{1}
+    keywords: ["§0", "§1", "§2", "§3", "§4", "§5", "§6", "§7", "§8", "§9", "§a", "§b", "§c", "§d", "§e", "§f", "§g", "§k", "§l", "§m", "§n", "§o"],
     contains: [
       {
         className: "string",
-        begin: "^",
-        end: "[A-Za-z0-9](?==)|(?=#)"
+        begin: "^[A-Za-z0-9]",
+        end: "[A-Za-z0-9](?==)|(?=#)",
+        contains: [
+          {
+            begin: "[A-Za-z0-9\\-_]+:[A-Za-z0-9\\-_]+",
+            end: "(?=\\.)"
+          }
+        ]
+      },
+      {
+        begin: "\\=",
+        end: "(?!.)|(?=#)",
+        contains: []
       },
       hljs.COMMENT("#", "$")
     ]
